345e57f50365e7f4032b925889d2b3551cc842a1
Wastabir Rajvee Alam <wastabirrajveealamsa_cse10@its.edu.in>
2018-03-25 18:14:52 -0400

    my first commit

---
+++ src/Start.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long startTime = 0;
+		long endTime=0;
+		System.out.println("Please enter 1 for nested join technique or 2 for sort based join");
+		Scanner scan =new Scanner(System.in);
+		int choice = Integer.parseInt(scan.nextLine());
+		startTime=System.currentTimeMillis();
+		if(choice==1){
+			
+		}
+		else{
+			
+			
+			
+			
+		}
+		endTime=System.currentTimeMillis();
+		System.out.println("Total time taken to run the process is "+(endTime-startTime)+"ms");
+	}
+
+}
